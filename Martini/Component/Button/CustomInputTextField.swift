e9a46627e5a9a6ba26c63de01a9deed88cd4378d
yi-sang <westernize@naver.com>
2021-08-15 15:58:22 +0900

    인풋 텍스트필드 레프트패딩 추가

--- Martini/Component/Button/CustomInputTextField.swift
+++ Martini/Component/Button/CustomInputTextField.swift
@@ -11,25 +11,30 @@ import UIKit
 
 class CustomInputTextField: UITextField  {
     var textName: String = ""
-
+    let iconView = UIImageView(frame:
+                                CGRect(x: 10, y: 5, width: 20, height: 20))
+    let iconContainerView: UIView = UIView(frame:
+                                            CGRect(x: 0, y: 0, width: 40, height: 30))
+        
     @objc func textFieldDidChange(textField: UITextField){
         
     }
     
     override func awakeFromNib() {
-        let iconView = UIImageView(frame:
-                                    CGRect(x: 10, y: 5, width: 20, height: 20))
-        let iconContainerView: UIView = UIView(frame:
-                                                CGRect(x: 20, y: 0, width: 30, height: 30))
         iconView.image = UIImage(systemName: "person.fill")
         iconView.tintColor = .black
+        
         iconContainerView.addSubview(iconView)
+//        iconContainerView.addSubview(leftPaddingView)
+//        leftPaddingView.backgroundColor = .black
+
         leftView = iconContainerView
         leftViewMode = .always
         placeholder = "이름"
+        
         self.backgroundColor = #colorLiteral(red: 0.7761746049, green: 0.7685815096, blue: 0.781961143, alpha: 1)
         self.layer.cornerRadius = 10
-
+        self.font = UIFont.boldSystemFont(ofSize: UIFont.labelFontSize)
         self.borderStyle = BorderStyle.none
         self.addTarget(self, action: #selector(textFieldDidChange(textField:)),
             for: UIControl.Event.editingChanged)
--- Martini/ViewController.swift
+++ Martini/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 class ViewController: UIViewController {
     
      @IBOutlet var category: BannerToggleButton!
+    @IBOutlet var iconInputField: CustomInputTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         // category.setColor(color: #colorLiteral(red: 0.7829886675, green: 0.4293426871, blue: 1, alpha: 1))
-        category.bannerButton.setImage(UIImage(named: "cocktail_icon.png"), for: .normal)
+//        category.bannerButton.setImage(UIImage(named: "cocktail_icon.png"), for: .normal)
+//        iconInputField.iconView.image = UIImage(systemName: "star")
         // Do any additional setup after loading the view.
     }
 
