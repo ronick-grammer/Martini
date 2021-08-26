d05149eed365a432f946dbb8344258f1050b0d78
yi-sang <westernize@naver.com>
2021-08-26 19:47:55 +0900

    토글버튼 통합

--- Martini/Component/Button/BannerToggleButton.swift
+++ Martini/Component/Button/BannerToggleButton.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 class BannerToggleButton: UIView {
-    let vstackView = UIStackView()
-    let buttonTitleLable = UILabel()
-    let banner = UIView()
-    let bannerButton = UIButton()
+    let vstackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 7
+        view.distribution = .fill
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var buttonTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "cocktail"
+        label.textAlignment = .center
+        label.clipsToBounds = true
+        label.layer.cornerRadius = 10
+        label.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
+        return label
+    }()
+    
+    let banner: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    let bannerButton: UIButton = {
+        let button = UIButton()
+        button.isEnabled = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var bannerImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "cocktail_icon"))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     var isClicked: Bool = false
 
     @objc func BannerToggleButtonHandler(_ sender: UIButton) {
@@ -20,61 +58,49 @@ class BannerToggleButton: UIView {
         if isClicked {
             // 붉은색
             banner.backgroundColor = #colorLiteral(red: 0.9400233626, green: 0.6212706566, blue: 0.6243276, alpha: 1)
-            buttonTitleLable.backgroundColor = #colorLiteral(red: 0.9400233626, green: 0.6212706566, blue: 0.6243276, alpha: 1)
+            buttonTitleLabel.backgroundColor = #colorLiteral(red: 0.9400233626, green: 0.6212706566, blue: 0.6243276, alpha: 1)
 
         } else {
             // 회색(기본)
-            banner.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
-            buttonTitleLable.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
-
-
+            banner.backgroundColor = #colorLiteral(red: 0.8110396266, green: 0.8110588193, blue: 0.8110484481, alpha: 1)
+            buttonTitleLabel.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
         }
     }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupLayout()
+    }
+    
+    func setupLayout() {
         self.addSubview(vstackView)
         bannerButton.addTarget(self, action: #selector(BannerToggleButtonHandler(_:)), for: .touchUpInside)
-        vstackView.axis = .vertical
-        vstackView.spacing = 7
-        vstackView.distribution = .fill
-        vstackView.translatesAutoresizingMaskIntoConstraints = false
-        vstackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        vstackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        vstackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        vstackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        
         vstackView.addArrangedSubview(banner)
-        banner.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
-        banner.layer.cornerRadius = 10
-        vstackView.addArrangedSubview(buttonTitleLable)
-        buttonTitleLable.text = "cocktail"
-        buttonTitleLable.textAlignment = .center
-        buttonTitleLable.clipsToBounds = true
-        buttonTitleLable.layer.cornerRadius = 10
-        buttonTitleLable.backgroundColor = #colorLiteral(red: 0.7637431026, green: 0.7688452601, blue: 0.7643030286, alpha: 1)
-        banner.addSubview(bannerButton)
-        bannerButton.isEnabled = true
-        bannerButton.translatesAutoresizingMaskIntoConstraints = false
-        bannerButton.topAnchor.constraint(equalTo: banner.topAnchor).isActive = true
-        bannerButton.leftAnchor.constraint(equalTo: banner.leftAnchor).isActive = true
-        bannerButton.rightAnchor.constraint(equalTo: banner.rightAnchor).isActive = true
-        bannerButton.bottomAnchor.constraint(equalTo: banner.bottomAnchor).isActive = true
-        bannerButton.clipsToBounds = true
-        bannerButton.layer.cornerRadius = 10
-
+        vstackView.addArrangedSubview(buttonTitleLabel)
+        banner.addSubview(bannerImage)
+        self.addSubview(bannerButton)
+        NSLayoutConstraint.activate([
+            vstackView.topAnchor.constraint(equalTo: self.topAnchor),
+            vstackView.widthAnchor.constraint(equalTo: self.widthAnchor),
+            vstackView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            bannerImage.topAnchor.constraint(equalTo: banner.topAnchor, constant: 10),
+            bannerImage.leftAnchor.constraint(equalTo: banner.leftAnchor, constant: 10),
+            bannerImage.rightAnchor.constraint(equalTo: banner.rightAnchor, constant: -10),
+            bannerImage.bottomAnchor.constraint(equalTo: banner.bottomAnchor, constant: -10),
+            bannerButton.topAnchor.constraint(equalTo: self.topAnchor),
+            bannerButton.widthAnchor.constraint(equalTo: self.widthAnchor),
+            bannerButton.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            banner.topAnchor.constraint(equalTo: self.topAnchor),
+            banner.widthAnchor.constraint(equalTo: self.widthAnchor),
+            banner.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -30)
+        ])
     }
     
     func setColor(color: UIColor){
-        buttonTitleLable.backgroundColor = color
+        buttonTitleLabel.backgroundColor = color
     }
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
