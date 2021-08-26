a50f29cb31197c551f351d136474b6f3e31d544e
asqw887 <asqw887@gmail.com>
2021-08-26 18:36:27 +0900

    Main 레이아웃 제작

---
+++ Martini/Component/Cell/MainTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.swift
+//  Martini
+//
+//  Created by 최예주 on 2021/08/22.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
