1bb0b4f835bba043eea5247fd62ea8315123bee6
yi-sang <westernize@naver.com>
2021-08-29 15:00:01 +0900

    프로젝트파일

--- Martini/LoginViewController.swift
+++ Martini/LoginViewController.swift
@@ -16,6 +16,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var loginButton: LoginButton!
     @IBOutlet var signupButton: LoginButton!
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.addKeyboardNotifications()
+        self.emailTextField.becomeFirstResponder()
+        print(type(of: self), #function)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         passwordTextField.isSecureTextEntry = true
@@ -36,9 +42,14 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         loginButton.informTextInfo(text: "로그인", fontSize: 30)
         signupButton.informTextInfo(text: "회원가입", fontSize: 30)
         print(type(of: self), #function)
-
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        self.removeKeyboardNotifications()
+        print(type(of: self), #function)
+    }
+    
+    // 로그인 버튼 액션
     @IBAction func loginButton(_ sender: LoginButton) {
         if flag == true {
             print(emailTextField.text!, passwordTextField.text!)
@@ -60,23 +71,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         CheckForLogin()
     }
     
-    // 화면을 터치했을 때 키보드가 내려가는 메소드
+    // 화면을 터치했을 때 키보드가 내려가는 메서드
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        self.addKeyboardNotifications()
-        self.emailTextField.becomeFirstResponder()
-        print(type(of: self), #function)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        self.removeKeyboardNotifications()
-        print(type(of: self), #function)
-    }
-    
-    //
+    // 로그인 조건에 따른 로그인 색을 변화하는 메서드
     func CheckForLogin() {
         if emailTextField.text?.contains("@") == true && passwordTextField.text?.count ?? 0 >= 6 {
             loginButton.setColor(color: #colorLiteral(red: 0.9405087233, green: 0.6196145415, blue: 0.6243818998, alpha: 1))
@@ -86,17 +86,13 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
             flag = false
         }
     }
-    // 뷰에 처음 들어왔을 때 키보드가 올라오는 메소드
-    //    override func viewWillAppear(_ animated: Bool) {
-    //            self.emailTextField.becomeFirstResponder()
-    //    }
     
     // 노티피케이션을 추가하는 메서드
     func addKeyboardNotifications(){
         // 키보드가 나타날 때 앱에게 알리는 메서드 추가
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification , object: nil)
         // 키보드가 사라질 때 앱에게 알리는 메서드 추가
-        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     // 노티피케이션을 제거하는 메서드
@@ -107,12 +103,15 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
+    // 리턴을 눌렀을 때 작동하는 메서드
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //        emailTextField.resignFirstResponder()
         if textField == emailTextField {
             passwordTextField.becomeFirstResponder()
+            signupButton(signupButton)
         } else {
-            passwordTextField.resignFirstResponder()
+//            passwordTextField.resignFirstResponder()
+            loginButton(loginButton)
         }
         return true
     }
@@ -126,28 +125,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         let keyboardHeight = keyboardRectangle.height
         
         if emailTextField.isEditing == true {
-            view.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - keyboardHeight - signupButton.frame.maxY)
+            view.transform = CGAffineTransform(translationX: 0, y: view.frame.height - keyboardHeight - signupButton.frame.maxY)
             //            keyboardAnimate(keyboardRectangle: keyboardRectangle, textField: emailTextField)
         } else if passwordTextField.isEditing == true {
-            view.transform = CGAffineTransform(translationX: 0, y: self.view.frame.height - keyboardHeight - signupButton.frame.maxY)
+            view.transform = CGAffineTransform(translationX: 0, y: view.frame.height - keyboardHeight - signupButton.frame.maxY)
         }
-        
-        // 키보드가 올라올때 화면을 올려주는 메소드
-        //            keyboardAnimate(keyboardRectangle: keyboardRectangle, textField: passwordTextField)
-        
-        //        if let keyboardFrame: NSValue = noti.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
-        //            let keyboardRectangle = keyboardFrame.cgRectValue
-        //            let keyboardHeight = keyboardRectangle.height
-        ////            self.view.frame.origin.y -= keyboardHeight
-        //    }
     }
-    
-    // 텍스트 필드가 가려졌을 때 키보드의 위치를 조정하는 메소드
-    //    func keyboardAnimate(keyboardRectangle: CGRect ,textField: UITextField){
-    //            if keyboardRectangle.height > (self.view.frame.height - textField.frame.maxY){
-    //                self.view.transform = CGAffineTransform(translationX: 0, y: (self.view.frame.height - keyboardRectangle.height - textField.frame.maxY))
-    //            }
-    //        }
     
     // 키보드가 사라졌다는 알림을 받으면 실행할 메서드
     @objc func keyboardWillHide(_ noti: NSNotification){
@@ -156,5 +139,10 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    
+    // 텍스트 필드가 가려졌을 때 키보드의 위치를 조정하는 메소드
+    //    func keyboardAnimate(keyboardRectangle: CGRect ,textField: UITextField){
+    //            if keyboardRectangle.height > (self.view.frame.height - textField.frame.maxY){
+    //                self.view.transform = CGAffineTransform(translationX: 0, y: (self.view.frame.height - keyboardRectangle.height - textField.frame.maxY))
+    //            }
+    //        }
 }
