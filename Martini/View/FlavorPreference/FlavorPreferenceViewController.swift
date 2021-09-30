d9fa6330f5caecc03f43bf253204fbaeaeb9dcb8
ronick-grammer <glflakcm@gmail.com>
2021-09-30 19:43:00 +0900

    칵테일 업데이트 수정, 맛 선택 타이틀 타입별 수정

--- Martini/View/FlavorPreference/FlavorPreferenceViewController.swift
+++ Martini/View/FlavorPreference/FlavorPreferenceViewController.swift
@@ -101,7 +101,14 @@ extension FlavorPreferenceViewController: UICollectionViewDataSource {
         // 각 맛의 서브 타이틀로 셀 만들기
         let taste = Cocktail.Taste.allCases[indexPath.row]
         let tasteInfo = getTasteInfo(taste: taste)
-        let subtitle = tasteInfo.subtitle
+        var subtitle = ""
+        
+        switch self.registrationType {
+        case .user    :
+            subtitle = tasteInfo.subtitle
+        case .cocktail:
+            subtitle = tasteInfo.title
+        }
         
         cell.configure(subtitle: subtitle, index: taste.index, registrationType: self.registrationType)
         
